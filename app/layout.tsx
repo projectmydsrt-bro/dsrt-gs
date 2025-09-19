b6283a6f5392dbb847bfd87ee5493ab16e90f271
projectmydsrt-bro <projectmydsrt@gmail.com>
2025-09-19 13:41:39 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "DSRT Simple",
-  description: "Next.js 14 + Tailwind starter tanpa error",
+  description: "Next.js 15 + Tailwind starter",
 };
 
 export default function RootLayout({
@@ -13,9 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-900">
-        {children}
-      </body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
